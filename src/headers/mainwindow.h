2722445b168974ec7d70e037c19f50c178fe021c
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2018-02-08 01:19:04 +0530

    added file selection dialog for loading reference and current images

--- src/headers/mainwindow.h
+++ src/headers/mainwindow.h
@@ -16,6 +16,11 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionLoad_Reference_Image_triggered();
+
+    void on_actionLoad_Current_Image_s_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
--- src/sources/mainwindow.cpp
+++ src/sources/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "src/headers/mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QStatusBar>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,4 +15,42 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionLoad_Reference_Image_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                     tr("Select Reference Image File"),
+                                                     "",
+                                                     "Images (*.png *.jpg *.jpeg *.tif *.tiff);;All Files (*)"
+                                                     );
+    QString status = tr("");
+    if (fileName.length() == 0) status = tr("No File Selected");
+    else status = tr("Selected ") + fileName;
+    statusBar()->showMessage(status, 2000);
+}
+
+void MainWindow::on_actionLoad_Current_Image_s_triggered()
+{
+    QStringList files = QFileDialog::getOpenFileNames(this,
+                                                      tr("Select Current Image(s)"),
+                                                      "",
+                                                      "Images (*.png *.jpg *.jpeg *.tif *.tiff);;All Files (*)"
+                                                      );
+    QString status;
+    switch (files.length()) {
+        case 0: {
+            status = tr("No File Selected");
+        } break;
+
+        case 1: {
+            status = tr("Selected file ") + files.at(0);
+        } break;
+
+        default: {
+            status = tr("Selected ") + tr(std::to_string(files.length()).c_str()) + tr(" Files : ")
+                    + files.first() + " ... " + files.last();
+        } break;
+    }
+    statusBar()->showMessage(status, 2000);
 }
